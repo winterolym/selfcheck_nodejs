788808581bee8e3201df52f9f04436c4339991df
winter <jsylego@naver.com>
2021-08-23 00:17:55 +0900

    added ssl

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var methodOverride = require('method-override');
+var https = require('https');
+var fs = require('fs');
 var app = express();
+
+const httpsServer = https.createServer({
+  key: fs.readFileSync('${process.env.SSLPATH}fullchain.cer'),
+  cert: fs.readFileSync('/home/ubuntu/.acme.sh/hahah.tech/hahah.tech.key'),
+}, app);
 
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended: true}));
@@ -12,7 +19,11 @@ app.use('/', function(req, res) {
 app.use('/api/execute', require('./routes/execute'));
 app.use('/api/get', require('./routes/get'));
 
-var port = 3001;
-app.listen(port, function(){
-  console.log('server on! http://localhost:'+port);
+httpsServer.listen(3001, () => {
+  console.log('HTTPS Server running on port 3001');
 });
+
+// var port = 3001;
+// app.listen(port, function(){
+//   console.log('server on! http://localhost:'+port);
+// });
