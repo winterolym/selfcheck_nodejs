fe011821ba1085793ac7ded37cf67c7b7d318149
winter <jsylego@naver.com>
2021-08-22 23:38:16 +0900

    Fixed ubuntu port 300 is already in use

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use('/api/execute', require('./routes/execute'));
 app.use('/api/get', require('./routes/get'));
 
-var port = 3000;
+var port = 3001;
 app.listen(port, function(){
   console.log('server on! http://localhost:'+port);
 });
