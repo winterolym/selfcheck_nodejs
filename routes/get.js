5ec86b09475753c34b827613a12c9b1db3186b94
winter <jsylego@naver.com>
2021-08-23 10:02:15 +0900

    all request that need body changed to post

--- routes/get.js
+++ routes/get.js
@@ -3,11 +3,11 @@ var hcs = require('hcs.js');
 var util = require('../util');
 var router = express.Router();
 
-router.get('/userinfo', util.login, async function(req, res){
+router.post('/userinfo', util.login, async function(req, res){
   const user_info = await hcs.userInfo(res.locals.endpoint, res.locals.password_token);
   res.status(200).json(user_info);
 });
-router.get('/token', util.login, function(req, res){
+router.post('/token', util.login, function(req, res){
   res.status(200).json({
     "login_token":res.locals.login_token,
     "password_token":res.locals.password_token
